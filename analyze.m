d95e01fc06bd800dd4071e95b92097548bf14660
baron0426 <baron0426@gmail.com>
2019-09-05 10:32:27 +0800

    synthesis by paperplane

--- analyze.m
+++ analyze.m
@@ -1,33 +1,33 @@
 clear all;
 close all;
 clc;
-[music, fs] = audioread('fmt.wav');
+[music, fs] = audioread('paperplane2.wav');
 size = length(music);
 A = [];
 unit = 400;
 for i = (unit+1):unit:(size-unit+1)
 a1 = max(abs(music((i-unit):(i-1))));
 a2 = max(abs(music(i:i+unit-1)));
-if(a2>=(a1*1.2))
+if(a2>=(a1*1.5))
 A = [A i];
 end
 end
 A = [A length(music)];
 B = [];
 partitionCnt = length(A)-1;
-B= [ 0     1     1     0     1     1     1     1     1     1     1     0     1     1     0     0     1     1     0     0     0     0     0     1     1     1     1     1     1     1     1     1    1     1     1     1     1     1     1     1     1];
+%B= [ 0     1     1     0     1     1     1     1     1     1     1     0     1     1     0     0     1     1     0     0     0     0     0     1     1     1     1     1     1     1     1     1    1     1     1     1     1     1     1     1     1];
 
 %test the automated split result
-%for k = 1:1:partitionCnt
- %   while 1
-  %     sound(music(A(k): A(k+1)),fs);
-   %     ok = input("OK? ");
-    %    if(ok == 0 || ok == 1)
-     %       B = [B ok];
-      %      break;
-       % end
-    %end
-%end
+for k = 1:1:partitionCnt
+    while 1
+       sound(music(A(k): A(k+1)),fs);
+        ok = input("OK? ");
+        if(ok == 0 || ok == 1)
+            B = [B ok];
+            break;
+        end
+    end
+end
 FREQ_INFO = zeros(partitionCnt,7);
 CRIT = 0.1;
 for k = 1:1:partitionCnt
@@ -38,4 +38,4 @@ for k = 1:1:partitionCnt
     end
 end
 FREQ_INFO( ~any(FREQ_INFO,2), : ) = [];  %rows
-makeWulingSong(16000, FREQ_INFO);
+%makeWulingSong(16000, FREQ_INFO);
