96b53734c7046400322a7529cb7afa7ee337fb41
baron0426 <baron0426@gmail.com>
2019-09-08 20:39:07 +0800

    findbaseFreqV2 OK
    findbaseFreqV2 OK

---
+++ analyzeV2.m
@@ -0,0 +1,27 @@
+clear all;
+close all;
+clc;
+[music, fs] = audioread('fmt.wav');
+sizee = length(music);
+partition = [];
+[yupper,ylower] = envelope(music,floor(fs/8),'peak'); %the order of FIR is an experience number
+axis_t = linspace(0,(sizee-1)/fs,sizee);
+env = yupper - ylower;
+plot(axis_t, yupper-ylower);
+[pks, loc] = findpeaks(env);
+loc2 = [0 loc' sizee];
+loc2size = length(loc2);
+for k = 1 : 1 : loc2size-1
+while 1
+dist = loc2(k+1) - loc2(k);
+start = loc2(k)+1 + 0.3*dist;
+start = floor(start);
+ending = loc2(k+1) - 0.3*dist;
+ending = floor(ending);
+sound(music(start:ending), fs);
+tmp = input("OK?");
+if (tmp == 1 || tmp == 0)
+    break
+end
+end
+end
