eaaddfb0575f2084fd2fe4ba48c7c5a645ad6224
baron0426 <baron0426@gmail.com>
2019-09-11 11:45:56 +0800

    q2 start

---
+++ q2.m
@@ -0,0 +1,11 @@
+clear all;
+load 'hall.mat';
+N = 8;
+n = 0 : 1: N-1;
+sample = hall_gray(1:N, 1:N);
+DCT = sqrt(2/N)*diag([sqrt(1/2), ones(1,N-1)])*cos(n'*(2*n + 1)*pi/(2*N));
+sample_DCT = DCT*sample*DCT';
+%sample_DCT2 = dct2(sample);
+
+%sample_DCT(1,1) = sample_DCT(1,1) - (128*8);
+%sample_IDCT  = DCT'*sample_DCT*DCT;
