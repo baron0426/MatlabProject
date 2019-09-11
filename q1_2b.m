0944d21badc6805e406821fda8080f89134eefe5
baron0426 <baron0426@gmail.com>
2019-09-11 09:47:27 +0800

    q1_2b ok
    q1_2b ok

--- q1_2b.m
+++ q1_2b.m
@@ -12,32 +12,26 @@ end
 if(y_left<8)
     hall_color(:,(pic_size(2)+1):(pic_size(2)+y_left),:) = repmat(hall_color(:,pic_size(2),:),1, y_left,1);
 end
-%determine the parameter of the circle
-xc = floor(pic_size(1)/2);
-yc = floor(pic_size(2)/2);
-r = floor(min(pic_size(1:2))/2);
+%determine the size of the block
+block_row = floor(pic_size(1)/8);
+block_col = floor(pic_size(2)/8);
+B = ones(block_row, block_col);
 
-%generate the relevent index according to the parameter
-R = 0:0.01:r;
-theta = 0:0.01:2*pi;
-X = xc + R'*cos(theta);
-Y = yc + R'*sin(theta);
-X = floor(X);
-Y = floor(Y);
-X(X==0) = 1;
-Y(Y==0) = 1;
-linearInd = sub2ind(pic_size(1:2),X,Y);
+%generating the 'chessboard'
+chessboard = [B, (1-B);(1-B), B];
+chessboard = repmat(chessboard,4,4);
+
 %generate the red circle
 hall_procR = hall_color(:,:,1);
 hall_procG = hall_color(:,:,2);
 hall_procB = hall_color(:,:,3);
 %cover the red circle on the original picture
-hall_procR(linearInd)=255;
-hall_procG(linearInd) = 0;
-hall_procB(linearInd) = 0;
+hall_procR(chessboard == 1) = 0;
+hall_procG(chessboard == 1) = 0;
+hall_procB(chessboard == 1) = 0;
 hall_proc = zeros(pic_size);
-hall_proc = u
-hall_proc(:,:,1)=hall_procR;
+hall_proc = uint8(hall_proc);
+hall_proc(:,:,1) = hall_procR;
 hall_proc(:,:,2) = hall_procG;
 hall_proc(:,:,3) = hall_procB;
 image(hall_proc);
