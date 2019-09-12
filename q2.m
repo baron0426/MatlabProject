22468f51be4582b7f32793577d24d2b55766f60d
BaronYou <baron0426@gmail.com>
2019-09-13 00:03:38 +0800

    DCT8andZigzagScan Inplemented
    DCT8andZigzagScan Inplemented

--- q2.m
+++ q2.m
@@ -15,11 +15,12 @@ pic_size = size(hall_gray);
 blockCntRow = floor(pic_size(1)/N);
 blockCntCol = floor(pic_size(2)/N);
 hall_gray_partition = mat2cell(double(hall_gray),N*ones(1,blockCntRow),N*ones(1,blockCntCol));
-n = 0 : 1: N-1;
-DCT = sqrt(2/N)*diag([sqrt(1/2), ones(1,N-1)])*cos(n'*(2*n + 1)*pi/(2*N));
-hall_gray_partition_DCT = cell(blockCntRow,blockCntCol);
-for k = 1:1:blockCntRow
-    for m  = 1:1:blockCntCol
-        hall_gray_partition_DCT{k,m} = DCT*hall_gray_partition{k,m}*DCT';
-    end
-end
+% n = 0 : 1: N-1;
+% DCT = sqrt(2/N)*diag([sqrt(1/2), ones(1,N-1)])*cos(n'*(2*n + 1)*pi/(2*N));
+hall_gray_partition_DCT = cellfun(@DCT8andZigzagScan, hall_gray_partition, 'UniformOutput', false);
+
+% for k = 1:1:blockCntRow
+%     for m  = 1:1:blockCntCol
+%         hall_gray_partition_DCT{k,m} = DCT*hall_gray_partition{k,m}*DCT';
+%     end
+% end
