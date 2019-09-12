25939b38641155686e0101d8e8f9f888df67cfb1
BaronYou <baron0426@gmail.com>
2019-09-12 15:26:38 +0800

    q2
    q2

--- q2.m
+++ q2.m
@@ -2,8 +2,10 @@ clear all;
 load 'hall.mat';
 N = 8;
 n = 0 : 1: N-1;
-sample = hall_gray(1:N, 1:N);
+sample = hall_gray(96+1:96+N, 40+1:40+N);
+sample = double(sample);
 DCT = sqrt(2/N)*diag([sqrt(1/2), ones(1,N-1)])*cos(n'*(2*n + 1)*pi/(2*N));
+
 sample_DCT = DCT*sample*DCT';
 %sample_DCT2 = dct2(sample);
 
