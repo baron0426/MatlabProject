102e19ecd496cdf718af886ad74d877cc41f02b0
baron0426 <baron0426@gmail.com>
2019-09-13 18:34:22 +0800

    decode OK
    decode OK

--- q2_11.m
+++ q2_11.m
@@ -41,10 +41,12 @@ ind = 1;
 AC_decode{m} = zeros(63,1);
 while(last<=code_length)
     if(AC_code(last:last+3) == logical([1,0,1,0]))
-        m = m + 1;
-        ind = 1;
-        AC_decode{m} = zeros(63,1);
         last = last+4;
+        if(last <= code_length)
+        	m = m + 1;
+            ind = 1;
+            AC_decode{m} = zeros(63,1);
+        end
     elseif(AC_code(last:last+10) == logical([1,1,1,1,1,1,1,1,0,0,1]))
         AC_decode{m}(ind:ind+15) = zeros(16,1);
         ind = ind + 16;
@@ -53,6 +55,14 @@ while(last<=code_length)
         for k = 2:1:max_sizecode_length
             query = find(~any((~(ACTAB(:,4:3+k) == AC_code(last:last+k-1))), 2));
             if(length(query) == 1)
+                if(query == 4 || query == 62)
+                    k = k + 1; %ZRL and EOB are not included in the ACTAB, 
+                               % which destroy the prefix encoding
+                               % property! It is essential to add this
+                               % conditioning!  HOW CAN YOU GIVE ME AN
+                               % INCOMPLETE ACTAB WITHOUT MENTIONING IN THE
+                               % INSTRUCTIONS?
+                end
                 run = ACTAB(query,1);
                 sizee = ACTAB(query,2);
                 last = last+k;
